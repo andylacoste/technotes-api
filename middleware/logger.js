0e310d56c457a7722f6d9f8170d71ace5aeb4213
amcniece1 <andrew.mcniece@gmail.com>
2024-02-19 18:52:13 -0800

    Updates for Logger + Controllers

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,18 +1,19 @@
-const {format} = require('date-fns')
-const {v4:uuid} = require('uuid')
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
 const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
+
 const logEvents = async (message, logFileName) => {
-    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
-    const logItem = `${dateTime}\t{uuid()}\t${message}\m`
+    const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss')
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
 
     try {
-        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))){
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
         }
         await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
-    }   catch (err) {
+    } catch (err) {
         console.log(err)
     }
 }
@@ -23,4 +24,4 @@ const logger = (req, res, next) => {
     next()
 }
 
-module.exports = {logEvents, logger}
+module.exports = { logEvents, logger }
